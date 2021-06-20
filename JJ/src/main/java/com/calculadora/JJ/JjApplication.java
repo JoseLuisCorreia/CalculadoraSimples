73b04cab965d784b3d0fd97c7b4dd925e6539f96
Jéssica Pinheiro <69260930+JessicaPinheiro@users.noreply.github.com>
2021-06-19 22:23:51 -0300

    Add files via upload

--- JJ/src/main/java/com/calculadora/JJ/JjApplication.java
+++ JJ/src/main/java/com/calculadora/JJ/JjApplication.java
@@ -15,17 +15,51 @@ public class JjApplication {
 		
 		Scanner leia = new Scanner(System.in);
 		
+		int digitOpcao;
+		
 		System.out.println("Ola você quer fazer qual tipo de calculo: \n1 - Soma \n2 - subtração \n3 - multiplicação \n4 - divisão");
 		calcular.operador = leia.nextInt();
 		
 		switch (calcular.operador) {
 		
 		case 1:
-			System.out.println(" + ");
+			System.out.println("Digite o preimiro numero que voê deseja calcular:");
+			calcular.val_1 = leia.nextFloat();
+			System.out.println("Digite o segundo numero:");
+			calcular.val_2 = leia.nextFloat();
+			
+			calcular.resultado = calcular.val_1 + calcular.val_2;
+			System.out.println("O resultado da soma é:" + calcular.resultado);
+			
+			System.out.println("Deseja fazer um novo calculo?\n");
+			System.out.println("Digite (1) - Sim\n(2) - Não ");
+			digitOpcao = leia.nextInt();
+			
+			
 			break;
+			
+			
+			
+			
 		case 2:
-			System.out.println(" - ");
+			System.out.println("Digite o preimiro numero que voê deseja calcular:");
+			calcular.val_1 = leia.nextFloat();
+			System.out.println("Digite o segundo numero:");
+			calcular.val_2 = leia.nextFloat();
+			
+			calcular.resultado = calcular.val_1 - calcular.val_2;
+			System.out.println("O resultado da soma é:" + calcular.resultado);
+			
+			System.out.println("Deseja fazer um novo calculo?\n");
+			System.out.println("Digite: \n(1) - Sim\n(2) - Não ");
+			
+			
+			
+			
 			break;
+			
+			
+			
 		case 3:
 
 			System.out.println("Digite o preimeiro valor : ");
@@ -52,7 +86,7 @@ public class JjApplication {
 			System.out.println("Seu resultado é " + calcular.resultado);
 			break;
 		}
-		return;
+		
 		
 	}
 
